e537c931d14c1f0fc0743a16ba7d630ecf6bc2bf
VictorSarov <victor.sarov@rentalmatics.com>
2026-06-24 08:16:51 +0100

    express server

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.get("/health", (req, res) => {
+    res.json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
