518c519e2b449c8893bd470fdf492c9d75b9a60e
VictorSarov <victor.sarov@rentalmatics.com>
2026-07-21 09:57:00 +0100

    all good

--- api/index.js
+++ api/index.js
@@ -1,31 +1,51 @@
-const express = require("express");
-const { WebSocketServer } = require("ws");
-const http = require("http");
+const express = require('express');
+const { WebSocketServer } = require('ws');
+const http = require('http');
 
 const PORT = 4000;
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
+const allClients = wss.clients;
+console.log("wss: ", wss);
 
-
-
-wss.on("connection", (ws) => {
+wss.on('connection', (ws) => {
+    console.log('============================');
     console.log('client connected');
 
-    
+    // on close
+    ws.on('close', () => {
+        console.log('---------------------------------');
+        console.log('Client disconnected');
+    });
 
+    // on message
+    ws.on('message', (message) => {
+        console.log('Message from client:', message.toString());
+    });
 
-})
+    // send message
+    ws.send('Hello from server last message');
 
+        setTimeout(() => {
+        ws.send('Hello from server 1');
+    }, 1000);
+
+    setTimeout(() => {
+        ws.send('Hello from server 2');
+    }, 4000);
+
+    // ws.close(1000, 'Goodbye');
+});
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
 
-app.get("/health", (req, res) => {
-    res.json({ status: "ok" });
+app.get('/health', (req, res) => {
+    res.json({ status: 'ok' });
 });
 
 server.listen(PORT, () => {
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,18 +1,49 @@
 import React, { useEffect, useRef } from 'react';
 
+const wsUrl = "ws://localhost:4000";
+
 function App() {
 
     const ws = useRef<WebSocket | null>(null);
 
+    const [wsMessage, setWsMessage] = React.useState<string>("");
+
     useEffect(() => {
-        ws.current = new WebSocket("ws://localhost:4000");
+        ws.current = new WebSocket(wsUrl);
+
+        console.log('ws', ws.current);
+
+        // open connection
+        ws.current.onopen = () => {
+            console.log("Connected to server");
+
+            ws.current?.send("Hello to server from client");
+           
+        };
+
+
+        ws.current.onmessage = (event) => {
+            console.log("Message from server:", event);
+            setWsMessage(event.data);
+        }
+
+        ws.current.onclose = (e) => {
+            console.log("Disconnected from server", e.reason);
+        }
+
+
+        // close connection code
+        return () => {
+            ws.current?.close();
+        }
+
     }, []);
 
 
 
   return (
     <div>
-      <h1>React app</h1>
+      <h1>React app: {wsMessage}</h1>
     </div>
   );
 }
