20f6af03629d6715157a081728a3dc60be95ed7f
VictorSarov <victor.sarov@rentalmatics.com>
2026-08-04 10:44:16 +0100

    all good

--- api/index.js
+++ api/index.js
@@ -1,18 +1,64 @@
-const express = require("express");
+const express = require('express');
+const { WebSocketServer } = require('ws');
+const http = require('http');
 
 const app = express();
 const PORT = 4000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+const httpServer = http.createServer(app);
+
+const wss = new WebSocketServer({ server: httpServer });
+
+function parseTheMessage(message) {
+    return message.toString();
+}
+
+// ws stuff
+wss.on('connection', (ws) => {
+    console.log('============================');
+    console.log('client connected');
+
+    ws.send('hello from server');
+
+    setTimeout(() => {
+        ws.send('hello from server 2 seconds after');
+    }, 2000);
+
+    ws.on('close', () => {
+        console.log('client disconnected');
+    });
+
+    ws.on('message', (message) => {
+        console.log(parseTheMessage(message));
+    });
+
+    ws.on('pong', () => {
+        console.log('pong received');
+    });
+
+    setInterval(() => {
+        console.log('ping sent');
+        ws.ping();
+    }, 3000);
+
+    // setTimeout(() => {
+    //     ws.close(1000, 'omg closed');
+    // }, 3000);
+
+    // end of ws code
 });
 
-app.get("/health", (req, res) => {
-    res.json({ status: "ok" });
+// Rest api
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
 
-app.listen(PORT, () => {
+app.get('/health', (req, res) => {
+    res.json({ status: 'ok' });
+});
+
+httpServer.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 function App() {
-  return (
-    <div>
-      <h1>React app</h1>
-    </div>
-  );
+
+    const [text, setText] = React.useState('no connection')
+
+    const ws = useRef<WebSocket | null>(null);
+
+    React.useEffect(() => {
+        ws.current = new WebSocket('ws://localhost:4000');
+
+        console.log('ws', ws.current);
+
+        ws.current.onopen = (e) => {
+            console.log('we are connected to server', e);
+
+            ws.current?.send('FROM CLIENT TO SERVER: connection established on client side, here is message');
+        };
+
+        ws.current.onmessage = (event) => {
+            console.log('============================');
+            console.log('event', event);
+            console.log('message', event.data);
+            setText(event.data)
+        };
+
+        ws.current.onclose = (e) => {
+            console.log('============================');
+            console.log('e', e);
+            console.log('message', e.reason);
+        };
+
+        return () => {
+            ws?.current?.close();
+        };
+    }, []);
+
+    return (
+        <div>
+            <h1>React app: {text}</h1>
+        </div>
+    );
 }
 
 export default App;
